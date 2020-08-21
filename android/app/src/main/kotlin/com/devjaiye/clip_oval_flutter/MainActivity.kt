75ff08279662dcbba4be3594c3778a9705131a16
Dayo Jaiye <dayojaiye74@gmail.com>
2020-08-21 01:07:05 +0100

    clip oval

---
+++ android/app/src/main/kotlin/com/devjaiye/clip_oval_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devjaiye.clip_oval_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: MyApp()
+      ));
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(50, 56, 70, 0.6),
+      body: Center(
+          child: SizedBox.fromSize(
+            size: Size(120.0, 120.0),
+            child: ClipOval(
+              child: Material(
+               color: Color.fromRGBO(50, 56, 75, 1.0),
+                elevation: 0.8,
+                child: InkWell(
+                  splashColor: Colors.deepOrange,
+                  onTap: (){},
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(Icons.favorite, color: Colors.white,
+                      size: 60.0,),
+                    ],
+                  ),
+                ),
+              )
+            ),
+          ),
+      ),
+    );
+  }
+}
+
